3a23af2c22cbc507a0d32551c0de750e064852e1
mayex-gif <alcaldealejo53@gmail.com>
2023-01-14 21:47:17 -0300

    url firebase agregada correctmente

--- src/main/java/com/portfolio/alca/Controller/PersonaController.java
+++ src/main/java/com/portfolio/alca/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend---prueba.web.app/")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
